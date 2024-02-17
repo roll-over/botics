7226c1c7f08a927781f39927f1fafc035792f308
dzigr <dmbrag@gmail.com>
2024-02-17 17:48:12 +0300

    Update message method in main

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from backend.messages import get_adapted_message
 from db import collection
 from dotenv import load_dotenv
 
@@ -18,9 +19,9 @@ dp = Dispatcher()
 
 
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
+async def start(message: types.Message):
     user = message.from_user
-    logging.info(f"Пользователь {message.from_user}")
+    logging.info(f"Пользователь {user}")
 
     user_data = {
         'user_id': user.id,
@@ -28,17 +29,11 @@ async def cmd_start(message: types.Message):
         'last_name': user.last_name,
         'username': user.username
     }
-    result = await collection.update_one({'user_id': user.id}, {"$set": user_data}, upsert=True)
-    logging.info(f"Result {result}")
-    await message.answer(
-        f"Hello, {message.from_user.first_name}! "
-        f"We're in progress of the development. We'll keep you in touch!"
-    )
+    await collection.update_one({'user_id': user.id}, {"$set": user_data}, upsert=True)
+    await message.answer(get_adapted_message(user.language_code, user.first_name))
 
 
 if __name__ == '__main__':
-    import asyncio
-
     loop = asyncio.get_event_loop()
     loop.create_task(dp.start_polling(bot))
     loop.run_forever()
