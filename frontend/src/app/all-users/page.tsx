7d537b815e9c33f383fb0c15791655e7fc0b3f4c
Timur Bondarenko <tibode495@gmail.com>
2024-02-17 21:14:56 +0600

    add list of users

---
+++ frontend/src/app/all-users/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function AllUsers() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (!users) {
+    return <p>No profile data</p>;
+  }
+
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl">All Users</h1>
+      <ul className="pl-5 list list-disc">
+        {users.map((user) => (
+          <li key={user.username}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
