4aefc6eeb64ef966bef7abeb1738c9141a38cc14
dzigr <dmbrag@gmail.com>
2024-02-17 20:04:12 +0300

    Update routing with new messages

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,18 @@ import asyncio
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters.command import Command
-from backend.messages import get_adapted_message
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from backend.messages import (
+    get_help_message,
+    get_support_message,
+    get_unacceptable_message,
+    get_welcome_message,
+)
 from db import collection
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -19,9 +26,9 @@ dp = Dispatcher()
 
 
 @dp.message(Command("start"))
-async def start(message: types.Message):
+async def welcome(message: types.Message):
     user = message.from_user
-    logging.info(f"Пользователь {user}")
+    logging.info(f"Пользователь {user.username}")
 
     user_data = {
         'user_id': user.id,
@@ -30,10 +37,50 @@ async def start(message: types.Message):
         'username': user.username
     }
     await collection.update_one({'user_id': user.id}, {"$set": user_data}, upsert=True)
-    await message.answer(get_adapted_message(user.language_code, user.first_name))
+    await message.answer(
+        await get_welcome_message(user.language_code, user.first_name),
+        reply_markup=types.ReplyKeyboardRemove(),
+    )
+
+
+@dp.message(Command("help"))
+async def commands(message: types.Message):
+    buttons = [
+        [
+            KeyboardButton(text='/help'),
+            KeyboardButton(text='/start'),
+            KeyboardButton(text='❤️'),
+        ],
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=buttons,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите комманду"
+    )
+    await message.answer(
+        await get_help_message(message.from_user.language_code),
+        reply_markup=keyboard,
+    )
+
+
+@dp.message(F.text == '❤️')
+async def unacceptable(message: types.Message):
+    await message.answer(
+        await get_support_message(message.from_user.language_code),
+        reply_markup=types.ReplyKeyboardRemove(),
+    )
+
+
+@dp.message()
+async def unacceptable(message: types.Message):
+    logging.info(f"Пользователь {message.text}")
+    await message.answer(
+        await get_unacceptable_message(message.from_user.language_code),
+        reply_markup=types.ReplyKeyboardRemove(),
+    )
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(dp.start_polling(bot))
+    loop.create_task(dp.run_polling(bot))
     loop.run_forever()
