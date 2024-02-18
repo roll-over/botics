c785b0537a816d636fff948e1849806d7f8a499f
Vladislav Lushnikov <adrenaline2142@gmail.com>
2024-02-18 14:01:12 +0500

    feat: add auth interface

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import Appbar from "./components/Appbar";
 import { Inter } from "next/font/google";
+import Providers from "./components/Providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Appbar />
+
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
